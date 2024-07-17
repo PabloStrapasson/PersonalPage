e4c5dbd149158d0cd206582708e2d873c91d4323
Pablo Strapasson <pablostrapasson98@gmail.com>
2024-07-17 16:10:11 -0300

    atualização de projetos

--- public/projects.ts
+++ public/projects.ts
@@ -10,7 +10,7 @@ const projects = {
             name: "API para Compree",
             description: "Projeto de uma API para uma loja virtual (Compree) desenvolvida utilizando o framework Nest.js que fornece uma arquitetura modularizada, o que possibilita o desenvolvimento de aplicações altamente escaláveis e de fácil manutenção. A aplicação foi escrita em typescript e utiliza o docker para a criação do servidor de banco de daods PostgreSQL.",
             repository: "https://github.com/PabloStrapasson/API-Loja-with-NestJS.git",
-            technologies: ["nestjs", "typescrpit", "postgresql", "docker"]
+            technologies: ["nestjs", "typescript", "postgresql", "docker"]
         },
         {
             name: "Adopet back-end",
